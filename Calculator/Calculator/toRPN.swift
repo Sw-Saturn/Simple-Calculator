4c67dbeab43c441f7c8bd736e3a32a52c5dda674
Kanta Demizu <demizu.1203@gmail.com>
2016-07-23 15:16:29 +0900

    create a project.

---
+++ Calculator/Calculator/toRPN.swift
@@ -0,0 +1,69 @@
+//
+//  toRPN.swift
+//  Calculator
+//
+//  Created by Kanta Demizu on 5/11/16.
+//  Copyright © 2016 mycompany. All rights reserved.
+//
+
+import Foundation
+class convertRPN  {
+    let opa = [
+        "^": (prec: 4, rAssoc: true),
+        "×": (prec: 3, rAssoc: false),
+        "÷": (prec: 3, rAssoc: false),
+        "+": (prec: 2, rAssoc: false),
+        "-": (prec: 2, rAssoc: false),
+        ]
+    
+    func rpn(tokens: [String]) -> [String] {
+        var rpn : [String] = []
+        var stack : [String] = [] // holds operators and left parenthesis
+        
+        for tok in tokens {
+            switch tok {
+            case "(":
+                stack += [tok] // push "(" to stack
+            case ")":
+                while !stack.isEmpty {
+                    let op = stack.removeLast() // pop item from stack
+                    if op == "(" {
+                        break // discard "("
+                    } else {
+                        rpn += [op] // add operator to result
+                    }
+                }
+            default:
+                if let o1 = opa[tok] { // token is an operator?
+                    for op in stack.reverse() {
+                        if let o2 = opa[op] {
+                            if !(o1.prec > o2.prec || (o1.prec == o2.prec && o1.rAssoc)) {
+                                // top item is an operator that needs to come off
+                                rpn += [stack.removeLast()] // pop and add it to the result
+                                continue
+                            }
+                        }
+                        break
+                    }
+                    
+                    stack += [tok] // push operator (the new one) to stack
+                } else { // token is not an operator
+                    rpn += [tok] // add operand to result
+                }
+            }
+        }
+        
+        return rpn + stack.reverse()
+    }
+    
+    func parseInfix(e: String) -> String {
+        let tokens = e.characters.split{ $0 == " " }.map(String.init)
+        print("CalArrayIs\'\(tokens)\'")
+        if tokens.last == "+" || tokens.last == "-" || tokens.last == "×" || tokens.last == "÷" {
+            return ""
+        }
+        else {
+            return rpn(tokens).joinWithSeparator(" ")
+        }
+    }
+}
